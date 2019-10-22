96a32de32a88b536d292ff4c0d3028cdafb0faf4
An Phan <antphan98@gmail.com>
2019-10-22 12:37:11 -0500

    added object storing q, a, value and gifs

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    const questions = [{
+            q: "1. Where did the Shiba Inu originate from?",
+            answers: ["Japan", "South Korea", "China", "Singapore"],
+            value: [true, false, false, false],
+            gif: "../images/shiba_inu.gif",
+
+        }, {
+            q: "2. What is the most popular dog breed?",
+            answers: ["Golden Retriever", "Labrador Retriever", "Dalmatian", "Pug"],
+            value: [false, true, false, false],
+            gif: "../images/lab_ret.gif",
+        }, {
+            q: "3. What is the scientific name for a dog?",
+            answers: ["Felis catus", "Abra kadabra", "Canis lupus", "Canis lupus familiaris"],
+            value: [false, false, false, true],
+            
+
+        }
+
+    ]
+
+
+
+});
